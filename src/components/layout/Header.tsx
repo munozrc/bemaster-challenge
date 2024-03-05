b5bf3453014f859ca1ce87e0471f441a317e3516
Carlos Muñoz <munozrengifocarlos@gmail.com>
2024-03-05 16:59:23 -0500

    feat: Change logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import { LogoutIcon } from "../icons";
+import logoApp from "@/assets/logo.png"
 
 export function Header () {
   return (
-    <div className="w-full h-14 px-5 bg-black">
+    <div className="w-full h-16 px-5 bg-zinc-900 lg:px-28">
       <header className="w-full h-full flex justify-between items-center">
         <h1 className="text-blue-300 font-bold">
-          <Link to="/">MoviesApp</Link>
+          <Link to="/">
+            <img src={logoApp} className="max-h-9"/>
+          </Link>
         </h1>
         <button className="text-slate-300 px-3">
           <LogoutIcon />
