41c5cc37c3259aefaf140abc8579b77664cb24c1
Carlos Muñoz <munozrengifocarlos@gmail.com>
2024-03-05 21:56:05 -0500

    feat: Create models

---
+++ src/models/content.model.ts
@@ -0,0 +1,6 @@
+export interface Content {
+  id: string,
+  title: string,
+  releaseYear: string,
+  category: string
+}
---
+++ src/models/user.model.ts
@@ -0,0 +1,9 @@
+export interface AuthUser {
+  email: string
+  password: string
+}
+
+export interface User {
+  email: string
+  profileImage: string
+}
