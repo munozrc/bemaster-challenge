c28867364b64bcce8f9d9907c165814b1e607a07
Carlos Muñoz <munozrengifocarlos@gmail.com>
2024-03-06 02:01:25 -0500

    feat: Create ContentCategory page

---
+++ src/components/icons/BackIcon.tsx
@@ -0,0 +1,19 @@
+export const BackIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="currentColor"
+    stroke="currentColor"
+    strokeWidth={0}
+    viewBox="0 0 512 512"
+  >
+    <path
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={48}
+      d="M244 400 100 256l144-144M120 256h292"
+    />
+  </svg>
+)
--- src/pages/ContentCategory.tsx
+++ src/pages/ContentCategory.tsx
@@ -1,7 +1,8 @@
-import { Navigate, useParams } from "react-router-dom";
+import { Link, Navigate, useParams } from "react-router-dom";
 import { PageContainer } from "@/components/layout"
 import { content, categories } from "@/mocks"
 import { Card } from "@/components";
+import { BackIcon } from "@/components/icons";
 
 export function ContentCategory () {
   const { slug } = useParams()
@@ -15,10 +16,31 @@ export function ContentCategory () {
 
   return (
     <PageContainer>
-      <div className="p-4 md:px-28 lg:px-44 bg-slate-800">
-        <h1>{currentCategory.name}</h1>
-      </div>
+      <section className="w-full flex relative h-40 overflow-hidden bg-slate-600">
+        <div className="w-full h-full absolute top-0 z-10 bg-black bg-opacity-20">
+          <div className="flex flex-col h-full justify-center px-4 md:px-28 lg:px-44">
+            <h1 className="text-3xl font-bold text-wrap text-slate-50">{currentCategory.name}</h1>
+            <span className="bg-slate-900 px-2 py-1 w-fit text-slate-300 text-xs rounded-md mt-1">Category</span>
+          </div>
+        </div>
+        <video
+          loop
+          muted
+          autoPlay
+          className="w-full object-cover blur"
+          src={`/video-categories/${currentCategory.slug}.mp4`}
+        />
+      </section>
       <main className="p-4 md:px-28 lg:px-44">
+        <header className="w-full flex justify-between items-center mb-4">
+          <div className="w-full flex flex-col">
+            <h3 className="font-semibold text-2xl">Content</h3>
+            <span className="text-sm text-slate-700">{listOfContent.length} result(s) found.</span>
+          </div>
+          <Link className="text-nowrap text-blue-500 hover:underline text-lg pr-2 flex items-center gap-2" to="/home">
+            <BackIcon />Go Back
+          </Link>
+        </header>
         <section className="grid gap-4 grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
           {listOfContent.map(({ id, poster }) => (
             <Card
