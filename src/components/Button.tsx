92fc0e5a82661f30660b259f481044398d93422a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2024-03-05 21:54:17 -0500

    feat: Show loading and error state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ interface Props extends ButtonHTMLAttributes
 export const Button: FC<Props> = ({ children, ...restOfProps }) => {
   return (
     <button 
-      className="rounded-md text-sm text-slate-100 font-semibold bg-black shadow hover:bg-slate-950/90 h-10 px-4 py-2"
+      className="flex items-center justify-center rounded-md text-sm text-slate-100 font-semibold bg-black shadow hover:bg-slate-950/90 h-10 px-4 py-2 disabled:opacity-90 disabled:cursor-not-allowed"
       {...restOfProps}
     >
       {children}
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,19 +1,23 @@
-import { FC, InputHTMLAttributes } from "react"
+import { InputHTMLAttributes, forwardRef } from "react"
 
 interface Props extends InputHTMLAttributes<HTMLInputElement>{
+  error?: string
   label?: string
 }
 
-export const InputField: FC<Props> = ({label, ...restOfProps}) => {
+export const InputField = forwardRef<HTMLInputElement,Props >(({label, error, ...restOfProps}, ref) => {
+  const isValidInput = error ? "border-red-500 outline-red-500" : ""
   return (
-    <div className="grid gap-2">
-      <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+    <div className="grid">
+      <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 mb-2">
         {label}
       </label>
-      <input
-        className="flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm"  
+      <input  
+        ref={ref}
+        className={`${isValidInput} flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm`}  
         {...restOfProps}
       />
+      <div className="min-h-5 text-red-500 text-sm">{error}</div>
     </div>
   )
-}
+})
