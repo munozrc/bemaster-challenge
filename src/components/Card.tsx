32513ffeffbdd474aa4a7d34efeb7d2467fbd7b6
Carlos Muñoz <munozrengifocarlos@gmail.com>
2024-03-06 02:50:09 -0500

    feat: Add lazy loading for images

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,6 +8,6 @@ interface Props {
 
 export const Card: FC<Props> = (props): JSX.Element => (
   <Link className="rounded-lg border overflow-hidden hover:scale-105 transition-all duration-150 ease-in" to={props.to}>
-    <img src={props.image} className="w-full object-cover"/>
+    <img src={props.image} className="w-full object-cover" loading="lazy"/>
   </Link>
 )
