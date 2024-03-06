6f5c85cb1c3be2a1e958581df98fe3a3d0225854
Carlos Muñoz <munozrengifocarlos@gmail.com>
2024-03-05 23:23:09 -0500

    refactor: Add more media queries

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import logoApp from "@/assets/logo.png"
 
 export function Header () {
   return (
-    <div className="w-full h-16 px-5 bg-zinc-900 lg:px-28">
+    <div className="w-full h-16 px-5 bg-zinc-900 md:px-28 lg:px-44">
       <header className="w-full h-full flex justify-between items-center">
         <h1 className="text-blue-300 font-bold">
           <Link to="/">
