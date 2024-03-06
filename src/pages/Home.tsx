fc6f057146198d5567df5d648319d8f7064da2b7
Carlos Muñoz <munozrengifocarlos@gmail.com>
2024-03-06 01:05:56 -0500

    feat: Change styles for Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,10 @@ export function Home () {
     <PageContainer>
       <div className="p-4 md:px-28 lg:px-44">
         <header className="py-2">
-          <h2 className="font-bold text-xl">Categories</h2>
+          <h2 className="font-semibold text-3xl">Welcome back!</h2>
+          <p className="text-md text-slate-700">
+            All ready? <strong className="text-sky-500">munozrengifocarlos@gmail.com</strong>
+          </p>
         </header>
         <section className="grid gap-4 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
           {categories.map((category) => (
@@ -21,7 +24,8 @@ export function Home () {
       </div>
       <main className="p-4 md:px-28 lg:px-44">
         <header className="pb-6">
-          <h2 className="font-bold text-3xl">All Content</h2>
+          <h2 className="font-semibold text-2xl">All Content</h2>
+          <p className="text-sm text-slate-700">Top picks for you. Updated daily.</p>
         </header>
         <section className="grid gap-4 grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
           {content.map(({ id, poster }) => (
